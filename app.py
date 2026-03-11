5423982f5bcbec3d47d28765412bb06945e40489
manoelvsalgado <manoelsalgadoneto@gmail.com>
2026-03-11 20:43:34 -0300

    add cache to data loading

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ st.set_page_config(
 )
 
 # --- Carregamento dos dados ---
-df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+@st.cache_data
+def carregar_dados():
+    return pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+
+df = carregar_dados()
 
 # --- Barra Lateral (Filtros) ---
 st.sidebar.header("🔍 Filtros")
